731d3d67a1ddf47a7ec5fca82839fe6c451829fa
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-10-30 20:25:56 +0100

    Added Task #3 - Christmas Tree

---
+++ src/main/java/pl/coderstrust/christmas/ChristmasTree.java
@@ -0,0 +1,32 @@
+package pl.coderstrust.christmas;
+
+public class ChristmasTree {
+
+    public static void main(String[] args) {
+
+    printChristmasTree(8);
+
+}
+
+    public static void printChristmasTree(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j <= size - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        for (int i = size; i <= size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2; k++) {
+                System.out.print("*");
+            }
+        }
+
+    }
+}
