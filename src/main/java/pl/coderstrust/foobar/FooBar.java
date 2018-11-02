95f6105d30a9b1e8f5555350a3e7ebe09659be04
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-11-02 20:59:36 +0100

    Correct Task FooBar

--- src/main/java/pl/coderstrust/foobar/FooBar.java
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -3,17 +3,16 @@ package pl.coderstrust.foobar;
 public class FooBar {
 
     public static void main(String[] args) {
-
-        for (int i = 0; i <= 100; i++) {
-            if (i != 0 && i % 15 == 0) {
-                System.out.println(i + " FooBar");
-            } else if (i != 0 && i % 3 == 0) {
-                System.out.println(i + " Foo");
-            } else if (i != 0 && i % 5 == 0) {
-                System.out.println(i + " Bar");
-            } else {
-                System.out.println(i);
+        System.out.println(0);
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+            if (i % 3 == 0) {
+                System.out.print("Foo");
             }
+            if (i % 5 == 0) {
+                System.out.print("Bar");
+            }
+            System.out.println();
         }
     }
 }
