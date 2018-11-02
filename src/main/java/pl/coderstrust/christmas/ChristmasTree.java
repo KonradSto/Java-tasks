780f3e00ec579ddfda3fd116e6d63ce98b5ae04e
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-11-02 21:10:07 +0100

    Correct Christmas Tree

--- src/main/java/pl/coderstrust/christmas/ChristmasTree.java
+++ src/main/java/pl/coderstrust/christmas/ChristmasTree.java
@@ -3,12 +3,11 @@ package pl.coderstrust.christmas;
 public class ChristmasTree {
 
     public static void main(String[] args) {
+        printChristmasTree(8);
+        printChristmasTree(15);
+    }
 
-    printChristmasTree(8);
-
-}
-
-    public static void printChristmasTree(int size) {
+    private static void printChristmasTree(int size) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j <= size - i; j++) {
                 System.out.print(" ");
@@ -17,16 +16,13 @@ public class ChristmasTree {
                 System.out.print("*");
             }
             System.out.println();
-
         }
         for (int i = size; i <= size; i++) {
             for (int j = 0; j < size; j++) {
                 System.out.print(" ");
             }
-            for (int k = 0; k < 2; k++) {
-                System.out.print("*");
-            }
+            System.out.print("**");
         }
-
+        System.out.println();
     }
 }
