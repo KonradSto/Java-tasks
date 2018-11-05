d3ad902a367a3f15804a7709600eca1d8d95ba28
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-11-05 20:40:14 +0100

    Correct Multiplication Table

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -7,21 +7,18 @@ public class MultiplicationTable {
         printMultiplicationTable(12);
     }
 
-    private static void printMultiplicationTable(int size) {
+    public static void printMultiplicationTable(int size) {
         System.out.print("      ");
         for (int i = 1; i <= size; i++) {
             System.out.printf("%6d", i);
         }
         System.out.println();
-        System.out.println();
-
         for (int i = 1; i <= size; i++) {
             System.out.printf("%6d", i);
             for (int j = 1; j <= size; j++) {
-                int equals = i * j;
-                System.out.printf("%6d", equals);
+                int result = i * j;
+                System.out.printf("%6d", result);
             }
-            System.out.println();
             System.out.println();
         }
     }
