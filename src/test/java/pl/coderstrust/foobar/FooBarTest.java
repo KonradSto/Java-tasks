6ede3bc29e657a4effadbc8509cb3a196e1d9c73
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-11-15 20:26:18 +0100

    FooBar Test - start project

---
+++ src/test/java/pl/coderstrust/foobar/FooBarTest.java
@@ -0,0 +1,17 @@
+package pl.coderstrust.foobar;
+
+import org.junit.Test;
+
+public class FooBarTest {
+
+    @Test
+    public void shouldPrintOutFooBar() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+}
