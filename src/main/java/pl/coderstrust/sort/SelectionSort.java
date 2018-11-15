d188752df2370591e510d1498d349fdc57aeffa1
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-11-15 21:04:58 +0100

    Selection Sort - temp correction

--- src/main/java/pl/coderstrust/sort/SelectionSort.java
+++ src/main/java/pl/coderstrust/sort/SelectionSort.java
@@ -6,23 +6,24 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] unsortedArray = {8, 9, -1, -5, 0};
         int[] sortedArray = sort(unsortedArray);
-        System.out.println(Arrays.toString(unsortedArray));
+        System.out.println(Arrays.toString(sortedArray));
     }
 
     public static int[] sort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            int minimum = array[i];
+        int[] arrayToSort = array.clone();
+        for (int i = 0; i < arrayToSort.length - 1; i++) {
+            int minimum = arrayToSort[i];
             int temp;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < minimum) {
+            for (int j = i + 1; j < arrayToSort.length; j++) {
+                if (arrayToSort[j] < minimum) {
                     temp = minimum;
-                    minimum = array[j];
-                    array[j] = temp;
+                    minimum = arrayToSort[j];
+                    arrayToSort[j] = temp;
                 }
-                array[i] = minimum;
+                arrayToSort[i] = minimum;
             }
         }
-        return array;
+        return arrayToSort;
     }
 
    /* private static void swapElementsInArray(int[] array, int currentMinimum, int indexOfCurrentElement) {
