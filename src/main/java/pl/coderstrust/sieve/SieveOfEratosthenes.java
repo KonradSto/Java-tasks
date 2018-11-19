e9c2ec90c5407fc717d5f97e8ec296b3ec471ca0
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-11-19 17:47:24 +0100

    Sieve - temp commit

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -3,6 +3,8 @@ package pl.coderstrust.sieve;
 import java.util.Arrays;
 
 public class SieveOfEratosthenes {
+    private final static int MARKER = 0;
+
     public static void main(String[] args) {
         int[] result = sieve(100);
         System.out.println(Arrays.toString(result));
@@ -11,14 +13,14 @@ public class SieveOfEratosthenes {
     public static int[] sieve(int maximumNumber) {
         int[] array = new int[maximumNumber];
         for (int i = 0; i <= maximumNumber - 2; i++) {
-            array[i] = i + 2;
+            array[i] = i;
         }
-        for (int i = 0; i < maximumNumber; i++) {
-            for (int n = 2; n <= maximumNumber; n++) {
-                if (array[i] % n == 0 && array[i] != n) {
-                    array[i] = 0;
-                }
+        for (int i = 2; i < maximumNumber; i++) {
+            // removeMultiplies(array, i)
+            for (int n = i + i; n < array.length; n += i) {
+                array[n] = 0;
             }
+            // private static void removeMultiplies(int[] arrayToClean, int number)
         }
         int primesCount = 0;
         for (int i = 0; i < maximumNumber - 2; i++) {
