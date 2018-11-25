20e2b77d42f8833d705f8c0531e5113664fac55e
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-11-25 14:48:47 +0100

    Multiplication Table - exception test added

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -2,11 +2,12 @@ package pl.coderstrust.multiplication;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        printMultiplicationTable(5);
-        printMultiplicationTable(9);
     }
 
-    public static int[][] printMultiplicationTable(int size) {
+    public static int[][] getMultiplicationTable(int size) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Size cannot be lower than 0");
+        }
         int[][] multiplicationTable = new int[size][size];
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= size; j++) {
--- src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -2,22 +2,9 @@ package pl.coderstrust.multiplication;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 
 public class MultiplicationTableTest {
-
-    @Test
-    public void checkIfReturnsMultiplicationTable1() {
-        //given
-        int size = 3;
-        int[][] expected = {{1, 2, 3}, {2, 4, 6}, {3, 6, 9}};
-
-        //when
-        int[][] array = MultiplicationTable.printMultiplicationTable(size);
-
-        //then
-        assertArrayEquals(expected, array);
-    }
 
     @Test
     public void checkIfReturnsMultiplicationTable2() {
@@ -26,9 +13,21 @@ public class MultiplicationTableTest {
         int[][] expected = {{1, 2, 3, 4, 5}, {2, 4, 6, 8, 10}, {3, 6, 9, 12, 15}, {4, 8, 12, 16, 20}, {5, 10, 15, 20, 25}};
 
         //when
-        int[][] array = MultiplicationTable.printMultiplicationTable(size);
+        int[][] array = MultiplicationTable.getMultiplicationTable(size);
 
         //then
         assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testExceptionMessage() {
+        //given
+        int size = -1;
+        try {
+            MultiplicationTable.getMultiplicationTable(size);
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException anIllegalArgumentException) {
+            assertEquals("Size cannot be lower than 0", anIllegalArgumentException.getMessage());
+        }
     }
 }
