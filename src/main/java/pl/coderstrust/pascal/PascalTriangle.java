bd7d3e4a3463b86612dac84f60bcc876cb053976
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-11-26 20:23:16 +0100

    Pascal Triangle Tests - initial commit

--- src/main/java/pl/coderstrust/pascal/PascalTriangle.java
+++ src/main/java/pl/coderstrust/pascal/PascalTriangle.java
@@ -1,22 +1,34 @@
 package pl.coderstrust.pascal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PascalTriangle {
     public static void main(String[] args) {
-        printPascalTriangle(8);
-        printPascalTriangle(12);
+        System.out.print(getPascalTriangle(5));
     }
 
-    private static void printPascalTriangle(int num) {
-        for (int i = 0; i < num; i++) {
-            for (int j = num; j > i; j--) {
-                System.out.print("   ");
-            }
-            int value = 1;
-            for (int k = 0; k <= i; k++) {
-                System.out.printf("%6d", value);
-                value = value * (i - k) / (k + 1);
-            }
-            System.out.println();
+    public static List<String> getPascalTriangle(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Size cannot be lower than 0");
         }
+        List<String> pascalTriangle = new ArrayList<>();
+        StringBuilder item = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            String spaces = "";
+            for (int j = num; j > i; j--) {
+                spaces += "  ";
+            }
+            item.append(spaces);
+            int intvalue = 1;
+            for (int k = 0; k <= i; k++) {
+                String value = String.format("%4d", intvalue);
+                item.append(value);
+                intvalue = intvalue * (i - k) / (k + 1);
+            }
+            pascalTriangle.add(item.toString());
+            item.delete(0, item.length());
+        }
+        return pascalTriangle;
     }
 }
---
+++ src/test/java/pl/coderstrust/pascal/PascalTriangleTest.java
@@ -0,0 +1,37 @@
+package pl.coderstrust.pascal;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+public class PascalTriangleTest {
+
+    @Test
+    public void shouldReturnChristmasTree() {
+        // Given
+        int num = 5;
+        List<String> expected = Arrays.asList("             1", "           1   1", "         1   2   1", "       1   3   3   1", "     1   4   6   4   1");
+
+        // When
+        List<String> actual = PascalTriangle.getPascalTriangle(num);
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldTestException() {
+        // Given
+        int num = -1;
+
+        // When
+        try {
+            PascalTriangle.getPascalTriangle(num);
+            Assert.fail("Exception should be thrown!");
+        } catch (IllegalArgumentException anIllegalArgumentException) {
+            assertEquals("Size cannot be lower than 0", anIllegalArgumentException.getMessage());
+        }
+    }
+}
