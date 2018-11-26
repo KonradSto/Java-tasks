922937aa0e434575aad6f1432e5f0454e0c31ebd
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-11-26 20:25:25 +0100

    FooBar Test - initial commit

--- src/test/java/pl/coderstrust/foobar/FooBarTest.java
+++ src/test/java/pl/coderstrust/foobar/FooBarTest.java
@@ -2,10 +2,8 @@ package pl.coderstrust.foobar;
 
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 public class FooBarTest {
