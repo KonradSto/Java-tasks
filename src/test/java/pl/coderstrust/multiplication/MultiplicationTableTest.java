b03d277469d0e6c2579f7b540dfd60ce5dbbb14b
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-11-28 21:23:43 +0100

    Multiplication Table - corrected exception test

--- src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -1,13 +1,18 @@
 package pl.coderstrust.multiplication;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
 public class MultiplicationTableTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void checkIfReturnsMultiplicationTable2() {
+    public void shouldReturnCorrectMultiplicationTable() {
         //given
         int size = 5;
         int[][] expected = {{1, 2, 3, 4, 5}, {2, 4, 6, 8, 10}, {3, 6, 9, 12, 15}, {4, 8, 12, 16, 20}, {5, 10, 15, 20, 25}};
@@ -20,14 +25,10 @@ public class MultiplicationTableTest {
     }
 
     @Test
-    public void testExceptionMessage() {
-        //given
+    public void shouldThrowExceptionForInvalidArgument() {
         int size = -1;
-        try {
-            MultiplicationTable.getMultiplicationTable(size);
-            fail("Expected an IllegalArgumentException to be thrown");
-        } catch (IllegalArgumentException anIllegalArgumentException) {
-            assertEquals("Size cannot be lower than 0", anIllegalArgumentException.getMessage());
-        }
+        thrown.expectMessage("Size cannot be lower than 0");
+        thrown.expect(IllegalArgumentException.class);
+        MultiplicationTable.getMultiplicationTable(size);
     }
 }
