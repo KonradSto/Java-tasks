4dbda029eb64d9652c70207cbaa1733fe32db2ab
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-12-16 12:16:55 +0100

    Sorting Methods - additional lines deleted

--- src/main/java/pl/coderstrust/sort/MergeSort.java
+++ src/main/java/pl/coderstrust/sort/MergeSort.java
@@ -41,4 +41,3 @@ public class MergeSort implements SortingMethod {
         }
     }
 }
-
--- src/main/java/pl/coderstrust/sort/QuickSort.java
+++ src/main/java/pl/coderstrust/sort/QuickSort.java
@@ -39,4 +39,3 @@ public class QuickSort implements SortingMethod {
         return r.nextInt((highIndex - lowIndex) + 1) + lowIndex;
     }
 }
-
