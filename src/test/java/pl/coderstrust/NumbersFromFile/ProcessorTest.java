b44817c6884f8ed1e39c25399d149ee15fea4d4e
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-12-18 21:36:59 +0100

    Processor Test - temp commit

--- src/test/java/pl/coderstrust/NumbersFromFile/ProcessorTest.java
+++ src/test/java/pl/coderstrust/NumbersFromFile/ProcessorTest.java
@@ -1,30 +1,37 @@
 package pl.coderstrust.NumbersFromFile;
 
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
 
 public class ProcessorTest {
 
     @Mock
     NumbersProcessor numbersProcessor;
+
     @Mock
     FileProcessor fileProcessor;
 
     @InjectMocks
     Processor processor;
 
-   /* @Test
-    public void shouldProcessProvidedInputFileAndSaveResultToProvidedOutputFile() {
+    @Test
+    public void shouldProcessProvidedInputFileAndSaveResultToProvidedOutputFile() throws FileNotFoundException {
         // given
-        when(fileProcessor.readLinesFromFile("fileName.txt")).thenReturn(Arrays.asList("1 2 3", "4 5 6");
+        when(fileProcessor.readLinesFromFile("fileName.txt")).thenReturn(Arrays.asList("1 2 3", "4 5 6"));
         when(numbersProcessor.processLine("1 2 3")).thenReturn("1+2+3=6");
 
         // when
         processor.process("fileName.txt", "output.txt");
 
-        // then
+       /* // then
         verify(fileProcessor).readLinesFromFile(...
         verify(numbersProcessor).processLine(....
-        verify(fileProcessor).writeLinesToFile(....
-    }*/
+        verify(fileProcessor).writeLinesToFile(....*/
+    }
 }
