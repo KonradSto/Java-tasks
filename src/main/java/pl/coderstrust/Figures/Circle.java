ef29c081c6a13f3d0f767fd258b4c0e574e60d71
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-12-20 20:42:28 +0100

    Figures - initial commit

---
+++ src/main/java/pl/coderstrust/Figures/Circle.java
@@ -0,0 +1,9 @@
+package pl.coderstrust.Figures;
+
+public class Circle implements Figure {
+
+    @Override
+    public double calculateArea() {
+        return 0;
+    }
+}
---
+++ src/main/java/pl/coderstrust/Figures/Rectangle.java
@@ -0,0 +1,16 @@
+package pl.coderstrust.Figures;
+
+public class Rectangle implements Figure {
+    private double a;
+    private double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public double calculateArea() {
+        return a * b;
+    }
+}
