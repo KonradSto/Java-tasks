e9f001a0a93e79a8b2d349a3796399e6ab6eddcf
Konrad Stolarczyk <stolarczyk.konrad@outlook.com>
2018-12-20 21:55:22 +0100

    Numbers From File - NumbersProcessor corrected

--- src/main/java/pl/coderstrust/NumbersFromFile/NumbersProcessor.java
+++ src/main/java/pl/coderstrust/NumbersFromFile/NumbersProcessor.java
@@ -1,42 +1,34 @@
 package pl.coderstrust.NumbersFromFile;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Scanner;
 
 public class NumbersProcessor {
     public String processLine(String line) {
-        line = line.replaceAll("[^0-9]+", " ");
-        List<String> stringList = new ArrayList<>(Arrays.asList(line.split(" ")));
-        stringList.remove(0);
-        List<Integer> integersList = new ArrayList<>(convertsStringArrayListToInteger(stringList));
-        StringBuilder builder = buildsFinalEquation(integersList);
+        if (!lineValidation(line)) {
+            return "";
+        }
+        if (lineValidation(line)) {
+            line = line.replaceAll("[^0-9]+", " ");
+        }
+        Scanner scan = new Scanner(line);
+        StringBuilder builder = new StringBuilder();
+        int result = 0;
+        while (scan.hasNextInt()) {
+            int nextInt = scan.nextInt();
+            builder.append(nextInt).append("+");
+            result += nextInt;
+        }
+        scan.close();
+        builder.deleteCharAt(builder.length() - 1);
+        builder.append("=").append(result);
         return builder.toString();
     }
 
-    private StringBuilder buildsFinalEquation(List<Integer> list) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < list.size() - 1; i++) {
-            builder.append(list.get(i)).append("+");
+    private boolean lineValidation(String line) {
+        if (line.equals("")) {
+            return false;
+        } else {
+            return true;
         }
-        builder.append(list.get(list.size() - 1));
-        builder.append("=").append(calculatesSumOfNumbersInList(list));
-        return builder;
-    }
-
-    private int calculatesSumOfNumbersInList(List<Integer> integersList) {
-        int sum = 0;
-        for (int i = 0; i < integersList.size(); i++) {
-            sum += integersList.get(i);
-        }
-        return sum;
-    }
-
-    private List<Integer> convertsStringArrayListToInteger(List<String> strList) {
-        List<Integer> intList = new ArrayList<>(strList.size());
-        for (String numberAsString : strList) {
-            intList.add(Integer.valueOf(numberAsString));
-        }
-        return intList;
     }
 }
