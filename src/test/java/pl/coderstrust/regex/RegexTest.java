4f33f831a29ca84b606350e71d49eedbcb8d0848
KonradSto <stolarczyk.konrad@outlook.com>
2018-12-28 22:54:52 +0100

    Regex - temp commit

--- src/test/java/pl/coderstrust/regex/RegexTest.java
+++ src/test/java/pl/coderstrust/regex/RegexTest.java
@@ -4,4 +4,23 @@ import static org.junit.Assert.*;
 
 public class RegexTest {
 
+    void test(){
+        String ipAddress;
+        for(){
+            for(){
+                for(){
+                    for(){
+                        ipAddress = String.format("%d.%d.%d.%d", i, j, k, l);
+
+                    }
+                }
+            }
+        }
+    }
+
+
+            "%d.1.1.1"
+            "1.%d.1.1"
+            "1.1.%d.1"
+            "1.1.1.%d"
 }
