fd90c16aab0b4c25dd2c4d78a6cff11e3d0a965d
KonradSto <stolarczyk.konrad@outlook.com>
2019-01-19 18:44:15 +0100

    implements Hoare partition (sorting does not work for large arrays)

--- src/main/java/pl/coderstrust/sort/QuickSort.java
+++ src/main/java/pl/coderstrust/sort/QuickSort.java
@@ -10,22 +10,27 @@ public class QuickSort implements SortingMethod {
 
     private void quickSort(int[] array, int lowIndex, int highIndex) {
         if (lowIndex < highIndex) {
-            int pivot = partition(array, lowIndex, highIndex);
-            quickSort(array, lowIndex, pivot - 1);
-            quickSort(array, pivot + 1, highIndex);
+            int partitionIndex = partition(array, lowIndex, highIndex);
+            quickSort(array, lowIndex, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, highIndex);
         }
     }
 
+
     private int partition(int[] array, int lowIndex, int highIndex) {
-        swap(array, lowIndex, getPivot(lowIndex, highIndex));
-        int border = lowIndex + 1;
-        for (int i = border; i <= highIndex; i++) {
-            if (array[i] < array[lowIndex]) {
-                swap(array, i, border++);
-            }
+        int pivot = getPivot(array, lowIndex, highIndex);
+        int i = lowIndex - 1;
+        int j = highIndex + 1;
+        while (true) {
+            do {
+                i++;
+            } while (array[i] < pivot);
+            do {
+                j--;
+            } while (array[j] > pivot);
+            if (i < j) swap(array, i, j);
+            else return j;
         }
-        swap(array, lowIndex, border - 1);
-        return border - 1;
     }
 
     private void swap(int[] array, int index1, int index2) {
@@ -34,8 +39,8 @@ public class QuickSort implements SortingMethod {
         array[index2] = temp;
     }
 
-    private int getPivot(int lowIndex, int highIndex) {
+    private int getPivot(int[] array, int lowIndex, int highIndex) {
         Random r = new Random();
-        return r.nextInt((highIndex - lowIndex) + 1) + lowIndex;
+        return array[r.nextInt((highIndex - lowIndex) + 1) + lowIndex];
     }
 }
